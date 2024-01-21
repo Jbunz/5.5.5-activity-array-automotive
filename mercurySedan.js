c2be23a8043603b51c9583a23aac1254115ccd37
Jbunz <jacob652@aol.com>
2024-01-20 18:01:34 -0800

    made corrections after viewing solution code.

--- mercurySedan.js
+++ mercurySedan.js
@@ -15,44 +15,50 @@ console.log(v.make);
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
 class Car extends VehicleModule.Vehicle {
-  constructor(
-    make,
-    model,
-    year,
-    color,
-    mileage,
-    maximumPassengers,
-    passengers,
-    numberOfWheels,
-    maximumSpeed,
-    fuel,
-    scheduleService
-  ) {
+  constructor(make, model, year, color, mileage) {
     super(make, model, year, color, mileage);
+  this.maximumPassengers = 5;
+  this.passengers = 0;
+  this.numberOfWheels = 4;
+  this.maximumSpeed = 160;
+  this.fuel = 10;
+  this.scheduleService = false;
+{
 
-    this.maximumPassengers = maximumPassengers;
-    this.passengers = passengers;
-    this.numberOfWheels = numberOfWheels;
-    this.maximumSpeed = maximumSpeed;
-    this.fuel = fuel;
-    this.scheduleService = scheduleService;
   }
 
   loadPassengers(num) {
-    if (this.passengers > this.maximumPassengers) {
+    if (this.passengers < this.maximumPassengers) {
+        if ((num + this.passenger) <= this.maximumPassengers) {
+            this.passenger = num;
+            return this.passenger;
+        } else {
+            console.log(this.model + " " + this.make + " not have enough space to take all passengers");
+
+        }
+    } else {
+        console.log(this.model + " " + this.make + " is full");
+    }
+        }
       this.availableRoom = true;
     }
-  }
+  
 
   start() {
     if (this.fuel > 0) {
-      this.start = true;
+        console.log("engine has started.");
+      return this.started = true
+    } else {
+        console.log("no fuel");
+        return this.started = false;
     }
-  }
+    }
+  
 
   scheduleService(mileage) {
     if (mileage > 30000) {
-      this.timeForMaintenance = true;
+      this.timeForMaintenance = true
+      return this.timeForMaintenance;
     }
   }
 }
